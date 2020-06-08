b2df11b6f829cfd7be836ec5363be3ffd5fc2de8
Franklyn Weber <franklyn@somacreate.com>
2020-06-08 10:39:40 +0100

    Initial Commit

---
+++ AnimationActions/AnimationActions.h
@@ -0,0 +1,19 @@
+//
+//  AnimationActions.h
+//  AnimationActions
+//
+//  Created by Franklyn on 08/06/2020.
+//  Copyright © 2020 Franklyn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AnimationActions.
+FOUNDATION_EXPORT double AnimationActionsVersionNumber;
+
+//! Project version string for AnimationActions.
+FOUNDATION_EXPORT const unsigned char AnimationActionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AnimationActions/PublicHeader.h>
+
+
